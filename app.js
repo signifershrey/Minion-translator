c9f7911ca56cc452428f4e2d190edf46926fc3e9
SHREY MAURYA <56041735+signifershrey@users.noreply.github.com>
2020-11-29 18:10:45 +0530

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+// var btnTranslate = document.querySelector("#btn-tanslate")
+
+// var textInput = document.querySelector("#txt-input");
+
+// //console.log(btnTranslate)
+
+// console.log(textInput);
+
+// btnTranslate.addEventListener("click",clickHandler())
+
+// function clickHandler() {
+//     console.log("Clicked!")
+//     console.log("input",textInput.value);
+// }
+
+alert("wakee up")
+prompt("page loaded")
+console.log("shrey is here")
