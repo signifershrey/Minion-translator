d66afe3f7371604384ef47f0cce7b218c57bb094
SHREY MAURYA <56041735+signifershrey@users.noreply.github.com>
2020-11-30 16:26:48 +0530

    update

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-// var btnTranslate = document.querySelector("#btn-tanslate")
+// alert("wakee up")
+// var a = prompt("Give your name?")
+// console.log("Welcome "+a);
+// console.log("shrey is here")
 
-// var textInput = document.querySelector("#txt-input");
 
-// //console.log(btnTranslate)
+ var btnTranslate = document.querySelector("#btn-translate")
 
-// console.log(textInput);
+ var textInput = document.querySelector("#txt-input");
 
-// btnTranslate.addEventListener("click",clickHandler())
+ var textOutput = document.querySelector("#output");
 
-// function clickHandler() {
+ 
+// btnTranslate.addEventListener("click",function clickEventHandler() {
+//     //console.log('Button is Clicked!')
+// })
+
+// function clickEventHandler() {
 //     console.log("Clicked!")
-//     console.log("input",textInput.value);
+
 // }
 
-alert("wakee up")
-prompt("page loaded")
-console.log("shrey is here")
+//console.log(textInput);
+
+//console.log(textOutput);
+
+
+btnTranslate.addEventListener("click",function clickhandler(){
+        console.log("Clicked");
+        console.log("Your Input:",textInput.value)
+        textOutput.innerHTML="IN BANANA LANGUAGE :  " +textInput.value;
+  
+})
+
+
+
+
+// 1)document.querySelector("textarea")
+// 2)document.querySelector(".btn-primary")
+// 3)document.querySelector("#input-btn")
+// 4)document.querySelector("input[name ='translator']")
