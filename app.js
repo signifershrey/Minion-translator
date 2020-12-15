5216f575de588b7789f608112db898f0fe67c2cc
SHREY MAURYA <56041735+signifershrey@users.noreply.github.com>
2020-12-15 14:30:05 +0530

    docs- update

--- app.js
+++ app.js
@@ -1,44 +1,33 @@
-
 var btnTranslate = document.querySelector("#btn-translate")
-
-var textInput = document.querySelector("#txt-input");
-
-var textOutput = document.querySelector("#txt-output");
+var textIn = document.querySelector("#txt-input");
+var textOut = document.querySelector("#txt-output");
 
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
-//var serverURL ="https://api.funtranslations.com/translate/valspeak.json"
-
-//Constructtheurl
 
 function getTranslationURL(input) {
-    return serverURL + "?" + "text" + input
+    return serverURL + "? text" + input
 }
 
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
+}
+function clickhandler() {
 
+    //taking input
+    var inputext = textIn.value;
+    var outputText ='';
 
-    function clickhandler() {
+    fetch(getTranslationURL(inputext))
+        .then(response => response.json())
+        .then(json => {
+            outputText = json.contents.translated;
+            textOut.value = outputText;
+        })
 
-        //taking input
-        var inputext = textInput.value;
+        .catch(errorHandler)
 
+}
 
-        //    if(inputext.length === 0){
-        //        alert("YOU NEED TO ENTER SOME TEXT")
-        //    }
-
-        fetch(getTranslationURL(inputext))
-            .then(response => response.json())
-            .then(json => {
-                var TranslatedText = json.contents.translated;
-                textOutput.innerText = TranslatedText;
-            })
-
-            .catch(errorHandler)
-
-    }
-
-    btnTranslate.addEventListener("click", clickhandler())
+btnTranslate.addEventListener("click", clickhandler)
